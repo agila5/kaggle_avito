c58617ae0791b45233cbf37c580d83038623d085
Riccardo Parviero <richardparrow@MacBook-Pro-di-Riccardo-2.local>
2018-05-11 17:08:25 +0200

    Wella, file apposti

--- data_import.R
+++ data_import.R
@@ -1,2 +1,10 @@
 #### Libraries
-library(tidyverse)
+library(readr)
+
+
+### Loading data from csv.zip
+avito_train = read_csv("train.csv.zip")
+
+
+### Saving as .Rdata
+save(avito_train, file = "my_avito")
